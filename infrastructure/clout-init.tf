87a09837296ebeec118d85ab7be71c0908c3c8e4
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2025-11-11 09:43:08 +0100

    chore: fix redundant cloud init file and remove host name

--- infrastructure/clout-init.tf
+++ infrastructure/clout-init.tf
@@ -14,7 +14,6 @@ resource "proxmox_virtual_environment_file" "cloud_config_server" {
 
   source_raw {
     data = templatefile("${path.module}/templates/user-data-server-cloud-config.yaml", {
-      hostname       = "k3s-server",
       ssh_public_key = trimspace(data.local_file.ssh_public_key.content)
       k3s_token      = random_password.k3s_token.result
       ip             = var.server_ip
@@ -24,18 +23,16 @@ resource "proxmox_virtual_environment_file" "cloud_config_server" {
 }
 
 resource "proxmox_virtual_environment_file" "cloud_config_agent" {
-  count        = 3
   content_type = "snippets"
   datastore_id = "local"
   node_name    = var.proxmox_node
 
   source_raw {
     data = templatefile("${path.module}/templates/user-data-agent-cloud-config.yaml", {
-      hostname       = "k3s-agent-${count.index + 1}",
       ssh_public_key = trimspace(data.local_file.ssh_public_key.content)
       k3s_token      = random_password.k3s_token.result
       server_ip      = var.server_ip
     })
-    file_name = "user-data-agent-cloud-config-${count.index}.yaml"
+    file_name = "user-data-agent-cloud-config.yaml"
   }
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,8 +5,7 @@ locals {
 
 # K3s Server VM
 module "k3s_server" {
-  source = "./modules/k3s-vm"
-
+  source             = "./modules/k3s-vm"
   name               = "k3sServer"
   node_name          = var.proxmox_node
   cloud_init_file    = proxmox_virtual_environment_file.cloud_config_server
@@ -17,12 +16,11 @@ module "k3s_server" {
 
 # K3s Agent VMs
 module "k3s_agent" {
-  source = "./modules/k3s-vm"
-  count  = 3
-
+  source             = "./modules/k3s-vm"
+  count              = 3
   name               = "k3sAgent${count.index}"
   node_name          = var.proxmox_node
-  cloud_init_file    = proxmox_virtual_environment_file.cloud_config_agent[count.index]
+  cloud_init_file    = proxmox_virtual_environment_file.cloud_config_agent
   ip_address         = "${var.agent_ips[count.index]}/24"
   gateway_ip_address = local.gateway_ip
   image_id           = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
