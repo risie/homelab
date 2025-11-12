863c3fce94cac0c37f13ffe1dc19fc853b375d4a
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2025-11-12 08:18:15 +0100

    fix: update image file name to qcow2
    This format is needed and specified in the tofu proxmox register docs

--- infrastructure/images.tf
+++ infrastructure/images.tf
@@ -3,5 +3,5 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   datastore_id = "local"
   node_name    = var.proxmox_node
   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-  file_name    = "jammy-server-cloudimg-amd64.img"
+  file_name    = "jammy-server-cloudimg-amd64.qcow2"
 }
