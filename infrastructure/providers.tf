c461dc77b4381bd18d51e7b51fb8b8bb64ce5a9b
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2026-07-23 19:44:14 +0200

    chore: update bpg/proxmox provider version

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.90.0"
+      version = "0.111.1"
     }
     random = {
       source  = "hashicorp/random"
